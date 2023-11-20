7449ffe56b747f7956a5e0cb91eb1c01fcb083e4
KUMKUMumrey91 <kumkumumrey@gmail.com>
2023-11-20 19:59:16 +0530

    frontend website done

---
+++ script.js
@@ -0,0 +1,26 @@
+gsap.from("#box4",{
+    duration:0.1,
+     scale:0.5,
+    scrollTrigger:{
+        trigger:"#box4",
+        scroller:"body",
+        // markers:true,
+        start:"top 65%",
+        end:"bottom 100%",
+        scrub:true,
+    },
+
+})
+gsap.from("#box8",{
+    duration:0.1,
+     scale:0.5,
+    scrollTrigger:{
+        trigger:"#box8",
+        scroller:"body",
+        // markers:true,
+        start:"top 65%",
+        end:"bottom 100%",
+        scrub:true,
+    },
+
+})
